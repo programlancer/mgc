1ab6cf221d10368463535cf1f166ee574ca43386
Victor <programlancer@gmail.com>
2017-08-14 10:35:33 +0300

    New project

---
+++ public/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=Cp1255">
+
+<link rel="stylesheet" type="text/css" href="./css/mgc.css">
+
+<title>Mgc</title>
+</head>
+<body>
+<header>
+<img alt="mgc" src="img/header.jpg">
+</header>
+<p>Body</p>
+<?php
+echo "Hello!"
+	?>
+
+<footer>
+<p>footer</p>
+</footer>
+</body>
+</html>
