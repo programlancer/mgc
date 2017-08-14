53a84a22a4e99da9e4dbc8c755d08ea632a02c1a
Victor <programlancer@gmail.com>
2017-08-14 13:00:48 +0300

    1300

--- public/index.php
+++ public/index.php
@@ -13,8 +13,46 @@
 </header>
 <p>Body</p>
 <?php
-echo "Hello!"
-	?>
+echo "Hello!"."<br>";
+
+$servername = "localhost";
+$username = "mgc";
+$password = "mgc";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully"."<br>";
+
+//var_dump($conn);
+
+
+$sql = "SELECT sign,model,serial FROM aircrafts";
+//var_dump($sql);
+
+$result = $conn->query($sql);
+
+//var_dump($result);
+
+if ($result) {
+	;
+	//if ($result->num_rows > 0) {
+//    // output data of each row
+//    while($row = $result->fetch_assoc()) {
+//        echo "Sign: ".$row["sign"]." Model: ".$row["model"]." Serial: ".$row["serial"]. "<br>";
+//    }
+//} else {
+//    echo "0 results";
+//}
+}
+else echo "No results";
+
+$conn->close();
+?>
 
 <footer>
 <p>footer</p>
